9f853e227db871bb5606fdef43a06a80dc858424
Jo <jo@labrys.io>
2024-04-26 14:45:38 +1000

    feat: guessing game

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+// Standard library, input/output
+use std::cmp::Ordering;
+use std::io;
+// Random number library
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Cast guess to string
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // If not a number, continue
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
